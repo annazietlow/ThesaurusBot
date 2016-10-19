be37ce67516c80f4b112537e5eae438063dc2a32
Anna Zietlow <azietlow@pnimedia.com>
2016-10-18 17:53:14 -0700

    implement basic thesaurus bot

--- ThesaurusBot/ThesaurusBot/Controllers/MessagesController.cs
+++ ThesaurusBot/ThesaurusBot/Controllers/MessagesController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Services.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
 
@@ -22,11 +23,15 @@ namespace ThesaurusBot
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                var query = ExtractSingleWord(activity.Text);
+                //                // calculate something for us to return
+                //                int length = (activity.Text ?? string.Empty).Length;
+                //
+                //                // return our reply to the user
+                //                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                var replyMessage = await GetSynonyms(query);
+                var reply = activity.CreateReply(replyMessage);
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
@@ -64,6 +69,43 @@ namespace ThesaurusBot
             }
 
             return null;
+        }
+
+        private async Task<string> GetSynonyms(string word)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-Mashape-Key", "vJD1NrdV8ZmshD9sN9KzxEbQRNvDp1L1C7ljsnherlwsVV7JAF");
+                var response = await client.GetAsync($"https://wordsapiv1.p.mashape.com/words/{word}/synonyms").ConfigureAwait(false);
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    return $"I'm sorry, '{word}' did not get a generate a proper response. Please try another word.";
+                }
+
+                var wordresponse = await response.Content.ReadAsAsync<WordResponse>().ConfigureAwait(false);
+
+                if (wordresponse.Synonyms.Length > 0)
+                {
+                    var messageResponse = "I have found the following synonyms:";
+
+                    //TODO: figure out how to do all this w/o silly string manipulation
+                    foreach (var synonym in wordresponse.Synonyms)
+                    {
+                        messageResponse = $"{messageResponse} {synonym},";
+                    }
+                    if (messageResponse.EndsWith(","))
+                    {
+                        messageResponse = messageResponse.Substring(0, messageResponse.Length - 1);
+                    }
+                    return messageResponse;
+                }
+                return $"I'm sorry, I could not find any synonyms for {word}.";
+            }
+        }
+
+        private static string ExtractSingleWord(string text)
+        {
+            return text.IndexOf(" ", StringComparison.Ordinal) > 0 ? text.Substring(0, text.IndexOf(" ", StringComparison.Ordinal)) : text;
         }
     }
 }
---
+++ ThesaurusBot/ThesaurusBot/WordResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThesaurusBot
+{
+    public class WordResponse
+    {
+        public string Word { get; set; }
+        public string[] Synonyms { get; set; }
+    }
+
+}
